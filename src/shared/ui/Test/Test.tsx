92297e7817dc201a36cfd68cf55f587a6f7b9f42
gorbatkoff <gorbatkovartyom@gmail.com>
2023-06-01 13:00:01 +0300

    Commit using

--- src/shared/ui/Test/Test.tsx
+++ src/shared/ui/Test/Test.tsx
@@ -1,11 +1,11 @@
-import React, { memo, ReactNode } from "react";
-import styles from "./Button.module.scss";
-import classNames from "classnames";
+import React, {memo} from "react";
 
 function Test() {
-  return <div>
-    Test component v2.3 dev Az
-    </div>; 
+    return (
+        <div>
+            Commit using git request-pull
+        </div>
+    );
 }
 
 export default memo(Test);
