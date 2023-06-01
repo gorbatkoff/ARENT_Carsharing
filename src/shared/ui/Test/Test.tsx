5b86e149b05374678f60dd4f297b5a06849c8d61
Croak-Croak <azamat.kalmykov07@gmail.com>
2023-06-01 11:35:22 +0300

    Add Test component

---
+++ src/shared/ui/Test/Test.tsx
@@ -0,0 +1,11 @@
+import React, { memo, ReactNode } from "react";
+import styles from "./Button.module.scss";
+import classNames from "classnames";
+
+function Test() {
+  return <div>
+    Test component
+  </div>;
+}
+
+export default memo(Test);
