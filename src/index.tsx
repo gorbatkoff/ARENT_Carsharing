d6efa66e1d1d21bdce9aca2bc6ae7fb8e07b037e
gorbatkoff <gorbatkovartyom@gmail.com>
2023-06-02 19:55:22 +0300

    Add BrowserRouter wrapper

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
-import {render} from "react-dom";
-import App from "./app/App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from "app/App";
+import {BrowserRouter} from "react-router-dom";
 import "app/styles/index.scss"
 
-render(
-    <App/>,
-    document.getElementById("root")
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>,
 )
