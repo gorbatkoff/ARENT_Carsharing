3ed9f6fb317c7e086e1ce0c52130cd8876c23812
Croak-Croak <azamat.kalmykov07@gmail.com>
2023-06-03 20:08:07 +0300

    Create Motorcycles widget

--- src/widgets/Motorcycles/Motorcycles.tsx
+++ src/widgets/Motorcycles/Motorcycles.tsx
@@ -1,6 +1,8 @@
 import {memo} from "react";
 import styles from "./Motorcycles.module.scss";
 import classNames from "classnames";
+import Text from "shared/ui/Text/Text";
+import TileLink from "shared/ui/TileLink/TileLink";
 
 interface MotorcyclesProps {
     className?: string
@@ -12,7 +14,13 @@ export const Motorcycles = memo((props: MotorcyclesProps) => {
 
     return (
         <div className={classNames(styles.Motorcycles, {}, [className])}>
-            Motorcycle
+             <Text title='Мотоциклы и мототехника'/>
+            <div className={classNames(styles.motoList)}>
+                <TileLink className={styles.motoListItem} url='#'>Мотоциклы</TileLink>
+                <TileLink className={styles.motoListItem} url='#'>Скутеры</TileLink>
+                <TileLink className={styles.motoListItem} url='#'>Мотовездеходы</TileLink>
+                <TileLink className={styles.motoListItem} url='#'>Снегоходы</TileLink>
+            </div>
         </div>
     );
 });
