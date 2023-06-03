7a688b5ee1bd92af8e24cda7580cd5b814e326ba
Croak-Croak <azamat.kalmykov07@gmail.com>
2023-06-03 15:31:39 +0300

    Create shared component (TileArticle)

---
+++ src/shared/ui/TileArticle/TileArticle.tsx
@@ -0,0 +1,43 @@
+import React, { memo, ReactNode, CSSProperties } from "react";
+import styles from "./TileArticle.module.scss";
+import classNames from "classnames";
+
+// export enum TileLinkTheme {
+//   PRIMARY = "primary",
+//   INFO = "info",
+// }
+
+// type Props = {
+//   children: ReactNode;
+
+// };
+interface TileLinkProps {
+  children: ReactNode;
+  url: string;
+  width: string | number;
+  height: string | number;
+  imageUrl: string;
+}
+
+function TileLink(props: TileLinkProps) {
+  const { children, url, width, height, imageUrl } = props;
+
+  const cls: CSSProperties = {
+    width,
+    height,
+  };
+
+  return (
+    <div className={classNames(styles.TileArticle)}>
+    <a className={classNames(styles.tileArticleImg)} style={cls} href={url}>
+        <img  src={imageUrl}></img>  
+    </a>
+    <a className={classNames(styles.tileArticleTitle)}  href={url}>
+        {children}
+    </a>
+    </div>
+ 
+  );
+}
+
+export default memo(TileLink);
