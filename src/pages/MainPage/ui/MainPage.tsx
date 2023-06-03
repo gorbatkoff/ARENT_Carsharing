8cfa2cc67d1bc4963ffd0153f4afce9e8b9a1981
gorbatkoff <gorbatkovartyom@gmail.com>
2023-06-03 17:45:46 +0300

    New styles, new shared component and fix Text component

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,19 +1,26 @@
-import { memo } from "react";
+import {memo} from "react";
 import styles from "./MainPage.module.scss";
 import classNames from "classnames";
-
+import {Container} from "shared/ui/Container/Container";
+import Text from "shared/ui/Text/Text";
 interface MainPageProps {
-  className?: string;
+    className?: string;
 }
 
 const MainPage = (props: MainPageProps) => {
-  const { className } = props;
+    const {className} = props;
 
-  return (
-    <div className={classNames(styles.MainPage, {}, [className])}>
-      Main page
-    </div>
-  );
+    return (
+        <div className={classNames(styles.MainPage, {}, [className])}>
+            <Container>
+                <Text
+                    title="Легковые автомобили в Москве"
+                    description="7 532 предложения"
+                />
+            </Container>
+
+        </div>
+    );
 };
 
 export default memo(MainPage);
