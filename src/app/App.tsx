da601924a8edefa40a493dbba8d402cc954d370c
Croak-Croak <azamat.kalmykov07@gmail.com>
2023-06-03 13:02:06 +0300

    Test shared component (TileLink)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Button, {ButtonTheme} from "shared/ui/Button/Button";
-
+import TileLink from 'shared/ui/TileLink/TileLink';
 function App() {
     return (
         <div className="App">
-            <Button theme={ButtonTheme.PRIMARY}>
+            {/* <Button theme={ButtonTheme.PRIMARY}>
                 Hello world
-            </Button>
+            </Button> */}
+            <TileLink width={"450px"} height={"450px"} url='https://github.com/gorbatkoff/production-project'>
+                Грузовики
+            </TileLink>
         </div>
     )
 }
