56b7839eeb8752061597a98e2c33cb9e6b7fe94a
gorbatkoff <gorbatkovartyom@gmail.com>
2023-06-03 17:02:59 +0300

    Add buttons and input

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -5,6 +5,8 @@ import BurgerMenu from "shared/assets/images/burger-menu-20-20.svg";
 import ArentLogo from "shared/assets/images/ARENT-TITLE.svg";
 import ArentTitle from "shared/assets/images/ARENT-LOGO.svg";
 import {Icon} from "shared/ui/Icon/Icon";
+import {Input} from "shared/ui/Input/Input";
+import Button, {ButtonTheme} from "shared/ui/Button/Button";
 
 interface HeaderProps {
     className?: string
@@ -17,12 +19,25 @@ export const Header = memo((props: HeaderProps) => {
     return (
         <div className={classNames(styles.Header, {}, [className])}>
             <div className={styles.Wrapper}>
-                <Icon Svg={BurgerMenu}/>
+
+                <Button theme={ButtonTheme.CLEAR}>
+                    <Icon Svg={BurgerMenu}/>
+                </Button>
 
                 <div className={styles.logo}>
-                    <Icon Svg={ArentTitle} />
-                    <Icon Svg={ArentLogo} />
+                    <Icon Svg={ArentTitle}/>
+                    <Icon Svg={ArentLogo}/>
                 </div>
+
+                <Input placeholder="Поиск объявлений" className={styles.Input}/>
+
+                <Button theme={ButtonTheme.INFO} className={styles.Button}>
+                    Войти
+                </Button>
+
+                <Button theme={ButtonTheme.PRIMARY} className={styles.Button}>
+                    Разместить объявление
+                </Button>
             </div>
         </div>
     );
