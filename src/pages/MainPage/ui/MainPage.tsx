d4b8a5b18460e5e585e83cfb3d31c9962708f388
Croak-Croak <azamat.kalmykov07@gmail.com>
2023-06-03 14:01:21 +0300

    Add TileLink component on the page

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,20 +1,28 @@
-import {memo} from "react";
+import { memo } from "react";
 import styles from "./MainPage.module.scss";
 import classNames from "classnames";
+import TileLink from "shared/ui/TileLink/TileLink";
 
 interface MainPageProps {
-    className?: string
+  className?: string;
 }
 
 const MainPage = (props: MainPageProps) => {
+  const { className } = props;
 
-    const {className} = props;
-
-    return (
-        <div className={classNames(styles.MainPage, {}, [className])}>
-            MainPage
-        </div>
-    );
+  return (
+    <div className={classNames(styles.MainPage, {}, [className])}>
+      <TileLink height={"237px"} width={"330px"} url={"https://google.com"}>
+        Грузовики
+      </TileLink>
+      <TileLink height={"237px"} width={"330px"} url={"https://google.com"}>
+        Легкие коммерческие
+      </TileLink>
+      <TileLink height={"237px"} width={"330px"} url={"https://google.com"}>
+        Строительная и дорожная
+      </TileLink>
+    </div>
+  );
 };
 
-export default memo(MainPage)
+export default memo(MainPage);
