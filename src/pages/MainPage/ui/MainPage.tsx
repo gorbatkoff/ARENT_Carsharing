8f3cb7ba78206a4a960f8c8419106625ccd7f992
Croak-Croak <azamat.kalmykov07@gmail.com>
2023-06-03 20:05:25 +0300

    Add new widgets and write styles

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,26 +1,33 @@
-import {memo} from "react";
+import { memo } from "react";
 import styles from "./MainPage.module.scss";
 import classNames from "classnames";
-import {Container} from "shared/ui/Container/Container";
+import { Container } from "shared/ui/Container/Container";
 import Text from "shared/ui/Text/Text";
+import { HandBook } from "widgets/HandBook/HandBook";
+import { Motorcycles } from "widgets/Motorcycles/Motorcycles";
+import { CommercialTransport } from "widgets/CommercialTransport/CommercialTransport";
 interface MainPageProps {
-    className?: string;
+  className?: string;
 }
 
 const MainPage = (props: MainPageProps) => {
-    const {className} = props;
+  const { className } = props;
 
-    return (
-        <div className={classNames(styles.MainPage, {}, [className])}>
-            <Container>
-                <Text
-                    title="Легковые автомобили в Москве"
-                    description="7 532 предложения"
-                />
-            </Container>
-
-        </div>
-    );
+  return (
+    <div className={classNames(styles.MainPage, {}, [className])}>
+      <Container>
+        <Text
+          title="Легковые автомобили в Москве"
+          description="7 532 предложения"
+        />
+      </Container>
+      <Container>
+        <CommercialTransport/>
+        <Motorcycles/>
+        <HandBook />
+      </Container>
+    </div>
+  );
 };
 
 export default memo(MainPage);
