2a2833a279a9330acaadacac73af431801134754
gorbatkoff <gorbatkovartyom@gmail.com>
2023-06-04 14:04:14 +0300

    Import Footer component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,16 +2,19 @@ import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 import {MainPage} from 'pages/MainPage'
 import {Header} from 'widgets/Header'
+import {Footer} from "widgets/Footer";
 
 
 function App() {
     return (
         <div className="App">
             <Header/>
-           
+
             <Routes>
                 <Route path={'/'} element={<MainPage/>}/>
             </Routes>
+
+            <Footer />
         </div>
     )
 }
