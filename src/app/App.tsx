34da0fd7a9937724694bbfce43ae6e3558f38de8
Croak-Croak <azamat.kalmykov07@gmail.com>
2023-06-04 14:49:45 +0300

    Add NavBar to Main page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import {Route, Routes} from 'react-router-dom'
-import {MainPage} from 'pages/MainPage'
-import {Header} from 'widgets/Header'
-import {Footer} from "widgets/Footer";
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { MainPage } from "pages/MainPage";
+import { Header } from "widgets/Header";
+import { Footer } from "widgets/Footer";
+import { NavigationBar } from "widgets/NavigationBar/ui/NavigationBar";
 
 function App() {
-    return (
-        <div className="App">
-            <Header/>
+  return (
+    <div className="App">
+      <Header />
+      <NavigationBar />
 
-            <Routes>
-                <Route path={'/'} element={<MainPage/>}/>
-            </Routes>
+      <Routes>
+        <Route path={"/"} element={<MainPage />} />
+      </Routes>
 
-            <Footer />
-        </div>
-    )
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
