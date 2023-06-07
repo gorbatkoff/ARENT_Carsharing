a0f299734c3bafce783eee00d3377bdc29b7403c
gorbatkoff <gorbatkovartyom@gmail.com>
2023-06-07 19:10:49 +0300

    Add Suspense wrapper

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,30 @@
-import React from "react";
+import React, {Suspense} from "react";
 import {Route, Routes} from "react-router-dom";
+
 import {MainPage} from "pages/MainPage";
+import {SearchPage} from "pages/SearchPage";
+import {NotFoundPage} from "pages/NotFoundPage";
+
 import {Header} from "widgets/Header";
 import {Footer} from "widgets/Footer";
 import {Navbar} from "widgets/Navbar";
-import {NotFoundPage} from "pages/NotFoundPage";
 
 function App() {
     return (
         <div className="App">
-            <Header/>
-            <Navbar/>
+            <Suspense fallback={""}>
 
-            <Routes>
-                <Route path={"/"} element={<MainPage/>}/>
-                <Route path={"*"} element={<NotFoundPage/>}/>
-            </Routes>
+                <Header/>
+                <Navbar/>
 
-            <Footer/>
+                <Routes>
+                    <Route path={"/"} element={<MainPage/>}/>
+                    <Route path={"/search"} element={<SearchPage/>}/>
+                    <Route path={"*"} element={<NotFoundPage/>}/>
+                </Routes>
+
+                <Footer/>
+            </Suspense>
         </div>
     );
 }
