0558ba47d45a73e9933f7db2bb3489169f4e34a7
gorbatkoff <gorbatkovartyom@gmail.com>
2023-06-07 19:10:58 +0300

    Add fileLoader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -34,9 +34,19 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         use: ['@svgr/webpack'],
     }
 
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+            {
+                loader: "file-loader",
+            },
+        ],
+    }
+
     return [
         typescriptLoader,
         cssLoader,
-        svgLoader
+        svgLoader,
+        fileLoader
     ]
 }
