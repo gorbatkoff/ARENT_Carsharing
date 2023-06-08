b98905dd46b26d59eaf8e557db8a48c9d3582b40
gorbatkoff <gorbatkovartyom@gmail.com>
2023-06-08 13:37:20 +0300

    New links

--- src/entities/MarksTable/model/mocks/brands.ts
+++ src/entities/MarksTable/model/mocks/brands.ts
@@ -1,70 +1,139 @@
 export const arrayOfBrands = [
-    { brand:'AUDI',
-        adCount:152, link:"/"},
-    {brand:'BMW',
-        adCount:152, link:"/"},
-    {brand:'Changan',
-        adCount:152, link:"/"},
-    {brand:'Chery',
-        adCount:152, link:"/"},
-    {brand:'Chevrolet',
-        adCount:152, link:"/"},
-    {brand:'Citroen',
-        adCount:152, link:"/"},
-    { brand:'Daewoo',
-        adCount:152, link:"/"},
-    {brand:'Ford',
-        adCount:152, link:"/"},
-    {brand:'Geely',
-        adCount:152, link:"/"},
-    {brand:'Haval',
-        adCount:152, link:"/"},
-    {brand:'Honda',
-        adCount:152, link:"/"},
-    {brand:'Hyundai',
-        adCount:152, link:"/"},
-    {brand:'Infinity',
-        adCount:152, link:"/"},
-    {brand:'Kia',
-        adCount:152, link:"/"},
-    {brand:'Land Rover',
-        adCount:152, link:"/"},
-    {brand:'Lexus',
-        adCount:152, link:"/"},
-    {brand:'Mazda',
-        adCount:152, link:"/"},
-    {brand:'Mercedes-Benz',
-        adCount:152, link:"/"},
-    {brand:'Mitsubishi',
-        adCount:152, link:"/"},
-    {brand:'Nissan',
-        adCount:152, link:"/"},
-    {brand:'Opel',
-        adCount:152, link:"/"},
-    {brand:'Peugeot',
-        adCount:152, link:"/"},
-    {brand:'Porsche',
-        adCount:152, link:"/"},
-    {brand:'Renault',
-        adCount:152, link:"/"},
-    {brand:'Skoda',
-        adCount:152, link:"/"},
-    {brand:'Subaru',
-        adCount:152, link:"/"},
-    {brand:'Suzuki',
-        adCount:152, link:"/"},
-    {brand:'Toyota',
-        adCount:152, link:"/"},
-    {brand:'Tesla',
-        adCount:152, link:"/"},
-    {brand:'Volkswagen',
-        adCount:152, link:"/"},
-    {brand:'Volvo',
-        adCount:152, link:"/"},
-    {brand:'ВАЗ (LADA)',
-        adCount:152, link:"/"},
-    {brand:'ГАЗ',
-        adCount:152, link:"/"},
-    { brand:'УАЗ',
-        adCount:152, link:"/"},
+    {
+        brand: 'AUDI',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'BMW',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Changan',
+        adCount: 152,
+        link: "/search"
+    },
+    {
+        brand: 'Chery',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Chevrolet',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Citroen',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Daewoo',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Ford',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Geely',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Haval',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Honda',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Hyundai',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Infinity',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Kia',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Land Rover',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Lexus',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Mazda',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Mercedes-Benz',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Mitsubishi',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Nissan',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Opel',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Peugeot',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Porsche',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Renault',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Skoda',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Subaru',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Suzuki',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Toyota',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Tesla',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Volkswagen',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'Volvo',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'ВАЗ (LADA)',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'ГАЗ',
+        adCount: 152, link: "/search"
+    },
+    {
+        brand: 'УАЗ',
+        adCount: 152, link: "/search"
+    },
 ];
